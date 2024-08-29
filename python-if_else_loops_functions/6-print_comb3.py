2be29d51fc7db0d81ce2a1f9e54f258e1de34edb
“Tristian <tristian.davis@atlasschool.com>
2024-08-29 00:21:06 -0500

    rerote fstring

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,6 @@ for first in range(0, 10):
 # second first+1 - 9
     for second in range(first+1, 10):
         if first == 8 and second == 9:
-            print(f"{first}{second}")
+            print("{}{}".format(first, second))
         else:
-            print(f"{first}{second}", end=", ")
-print() # add newline?
+            print("{}{}".format(first, second), end=", ")
