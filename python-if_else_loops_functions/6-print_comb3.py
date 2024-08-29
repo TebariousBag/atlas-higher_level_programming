512bf2062b4f1871ca57956f4513a1f996f380b7
“Tristian <tristian.davis@atlasschool.com>
2024-08-29 00:22:15 -0500

    removed comments

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # print all possible combos of 2 digits
-# first 0-9
 for first in range(0, 10):
-# second first+1 - 9
     for second in range(first+1, 10):
         if first == 8 and second == 9:
             print("{}{}".format(first, second))
