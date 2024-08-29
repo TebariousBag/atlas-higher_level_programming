ae37de583a32c1a6ac2d6e97196d89ddf2612431
“Tristian <tristian.davis@atlasschool.com>
2024-08-28 23:12:00 -0500

    last digit test

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = "Last digit of"
+str2 = "and is greater than 5"
+str3 = "and is 0"
+str4 = "and is less than 6 and not 0"
+if number >= 0:
+    lastdigit = number % 10
+else:
+    lastdigit = number % (-10)
+if lastdigit == 0:
+    print(f'{str1} {number} is {lastdigit} {str3}')
+elif lastdigit > 5:
+    print(f'{str1} {number} is {lastdigit} {str2}')
+elif lastdigit < 6 != 0:
+    print(f'{str1} {number} is {lastdigit} {str4}')
+    
