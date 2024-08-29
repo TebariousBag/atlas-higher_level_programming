d804679853920175edf2cca0b960935724300ab3
“Tristian <tristian.davis@atlasschool.com>
2024-08-29 00:59:46 -0500

    2 spaces for in line comments

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    newstr = "" # create empty string
+    newstr = ""  # create empty string
     for letter in str:
-        upper = ord(letter) # turn numeric
-        upper = upper - 32 # turn to upper
+        upper = ord(letter)  # turn numeric
+        upper = upper - 32  # turn to upper
         if letter >= 'a' and letter <= 'z':
-            newstr = newstr + chr(upper) # add the eppercase character
+            newstr = newstr + chr(upper)  # add the eppercase character
         else:
-            newstr = newstr + letter # add letter to string
-    print("{}".format(newstr)) # print string
+            newstr = newstr + letter  # add letter to string
+    print("{}".format(newstr))  # print string
