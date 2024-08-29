ef6241a24e1cf4ee46dcc2a16ad5a2742aa5048a
“Tristian <tristian.davis@atlasschool.com>
2024-08-28 23:15:25 -0500

    added comments

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,10 @@ str1 = "Last digit of"
 str2 = "and is greater than 5"
 str3 = "and is 0"
 str4 = "and is less than 6 and not 0"
+#need to get last digit
 if number >= 0:
     lastdigit = number % 10
+#negative last digit
 else:
     lastdigit = number % (-10)
 if lastdigit == 0:
