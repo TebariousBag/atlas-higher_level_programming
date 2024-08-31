0379573aa4a445e9d0daea4bec3673edacc00221
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 18:32:15 -0500

    sum args

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     args = sys.argv  # get args[]
     total = 0  # holder for total
     
-    for arg in args:
-        total += int(arg)
+    if len(args) > 1:  # if more than 1 arg
+        for arg in args:
+            total += int(arg)
     print(f"{total}")
     
