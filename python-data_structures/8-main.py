84f36bff5dcb85f314d658e8725c351bd76a0e52
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 23:53:24 -0500

    deleted an extra line from 8main

--- python-data_structures/8-main.py
+++ python-data_structures/8-main.py
@@ -4,5 +4,3 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
-
-guillaume@ubuntu:~/$ ./8-main.py
