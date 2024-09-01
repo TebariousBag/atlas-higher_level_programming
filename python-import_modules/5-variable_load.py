ceeb33f5395641d5140e0beab9787e7745b6744b
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 19:00:37 -0500

    comment fix, newline

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a  #import a from module name
+    from variable_load_5 import a  # import a from module name
 
     print(f"{a}")  # print a
-    
