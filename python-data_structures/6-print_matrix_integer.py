71897b7b34df755e72d55454d835ad574d815683
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 22:16:49 -0500

    end=" "

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
 	for row in range(len(matrix)):  # iterate rows col's
 		for col in range(len(matrix[row])):
-			print("{:d}".format(matrix[row][col]), end="")
+			print("{:d}".format(matrix[row][col]), end=" ")
 		print()  # newline
