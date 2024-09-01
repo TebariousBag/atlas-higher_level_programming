8e9994c44feb8348166a6ed841444b352c64689f
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 21:43:26 -0500

    c's

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new = ""
+    c = ""  # saving c's
     for i in my_string:
-        if i == 'c' or i == 'C':
-            return (None)
+        if i == 'c' or i == 'C':  # save c's here
+            c += i
         else:
-            new += i
+            new += i  # str w/o c's
         return (new)
