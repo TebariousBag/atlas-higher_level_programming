c7df028cfedb6881a85444797b22f02a5e50f2f1
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 22:27:15 -0500

    indentations

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	for row in range(len(matrix)):  # iterate rows col's
-		for col in range(len(matrix[row])):
-			if col == len(matrix[row]) -1:  # end of row
-				print("{:d}".format(matrix[row][col]), end="")
-			print("{:d}".format(matrix[row][col]), end=" ")
-		print()  # newline
+    for row in range(len(matrix)):  # iterate rows col's
+        for col in range(len(matrix[row])):
+            if col == len(matrix[row]) -1:  # end of row
+                print("{:d}".format(matrix[row][col]), end="")
+            else:
+                print("{:d}".format(matrix[row][col]), end=" ")
+        print()  # newline
