0c8c4924fa7e91645bbfb2547af340edba183e62
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 21:01:20 -0500

    reversed()

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    rev = reversed(my_list)  # reverse string
+    
+    for i in rev:  # iterate and print
+        print("{:d}".format(i))
