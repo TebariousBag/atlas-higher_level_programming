7bd0bafaf0d77065b1b4732307ee9e67a2f9ae95
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 22:24:13 -0500

    end of row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
 	for row in range(len(matrix)):  # iterate rows col's
 		for col in range(len(matrix[row])):
+			if col == len(matrix[row]) -1:  # end of row
+				print("{:d}".format(matrix[row][col]), end="")
 			print("{:d}".format(matrix[row][col]), end=" ")
 		print()  # newline
