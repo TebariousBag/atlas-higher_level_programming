30d094280d1aeee52dd9e7bd458559290ad80725
“Tristian <tristian.davis@atlasschool.com>
2024-08-31 20:45:51 -0500

    space

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) -1:  #if neg or out of range return og
+    if idx < 0 or idx > len(my_list) -1:  # if neg or out of range return og
         return (my_list)
     else:
         my_list[idx] = element  # replace element at idx
