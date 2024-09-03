58c3ac3378ca7856ebf3d9f8fccf1cd687706db3
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 13:29:28 -0500

    indent

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     # empty list
     for element in my_list:
-    # looking for search, and replacing with replace
+        # looking for search, and replacing with replace
         if element == search:
             new_list.append(replace)
     # otherwise just append the element
