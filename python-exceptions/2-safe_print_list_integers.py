65f5d9546f0a30bc109b71039f6db1e74e7a92f7
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 16:04:51 -0500

    space

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     # print value of int
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end=" ")
+            print("{:d}".format(my_list[i]), end="")
             counter += 1
         except (TypeError, ValueError):
         # dont print if value or type error
