805dd1b0c5720d09c977b0b87bedc6071a09c14b
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 15:59:40 -0500

    continue?

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     # print value of int
-    for i in range(0, x):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i], end=""))
             counter += 1
         except (TypeError, ValueError):
         # dont print if value or type error
-            pass
+            continue
     print("")
         # newline
     return (counter)
