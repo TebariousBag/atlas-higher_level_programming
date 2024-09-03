aa0c6ad885b06ab9d17a250e6aee60f3f076863e
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 16:28:34 -0500

    division BY 0

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div = 0
         except (ZeroDivisionError):
-            print("division of 0")
+            print("division by 0")
             div = 0
         finally:
             new.append(div)
