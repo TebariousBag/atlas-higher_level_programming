0461dc2fd109bcf11822957f301462f96da6fb9e
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 14:54:26 -0500

    didnt read max 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    # takes lambda func to multiply x
-    # map applies to each element
-    # then makes a list
-    multiplied = (list(map(lambda x: x * number, my_list)))
-    return (multiplied)
+    return ((list(map(lambda x: x * number, my_list))))
