e1040f420e820074c43056864d172916c5850b6a
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 13:08:10 -0500

    spacing

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new = [[num ** 2 for num in row] for row in matrix]
     return (new)
-  # square num, and iterate through row
-  # then iterate through the next row
+    # square num, and iterate through row
+    # then iterate through the next row
