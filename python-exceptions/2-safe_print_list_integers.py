068edcd925706d505760c8711bf48ed2980c22aa
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 15:46:33 -0500

    changed to i

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     # print value of int
-    for int in range(x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[int], end=""))
+            print("{:d}".format(my_list[i], end=""))
             counter += 1
         except (TypeError, ValueError):
         # dont print if value or type error
