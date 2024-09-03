a02fc0ed689460bdfa499abcf3bfeae14dcc4963
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 14:14:51 -0500

    sorted key

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted = list(a_dictionary.keys())
+    # get list of keys
+    sorted.sort()
+    # sort them
+    for key in sorted:
+        print("{}:{}".format(key, a_dictionary[key]))
+        # print top level key, then lower key
