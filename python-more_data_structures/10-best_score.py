f6be49b944b076f083e5000ec53c9d3a85b565c9
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 14:46:33 -0500

    moved declaration

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = max(a_dictionary, key=a_dictionary.get)
     if a_dictionary:
-        return (max_value)
+        score = max(a_dictionary, key=a_dictionary.get)
+        return (score)
     # get the key from adict that is max value
     return (None)
