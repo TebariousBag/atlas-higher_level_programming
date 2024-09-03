55d2910a1520899974614635ea7c326b45f98215
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 15:20:22 -0500

    moved block position

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,14 +2,14 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     # in range of x
-    for counter in range(x):
+    for idx in range(x):
         try:
             # print element of list
-            print(f"{my_list[counter]}")
+            print(f"{my_list[idx]}")
             counter += 1
             # increase counter
         except IndexError:
             # if encountering an error break
             break
-        print()
-        return (counter)
+    print()
+    return (counter)
