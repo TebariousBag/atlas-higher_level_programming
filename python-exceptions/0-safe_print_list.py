d3d75e53b0b472f6e59529c29878b82de061160c
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 15:21:32 -0500

    end?

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             # print element of list
-            print(f"{my_list[idx]}")
+            print(f"{my_list[idx]}, end=""")
             counter += 1
             # increase counter
         except IndexError:
