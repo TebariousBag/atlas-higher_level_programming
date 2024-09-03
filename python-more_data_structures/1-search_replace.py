59749244b6d7033f5ee6c4cd595157d8242b4223
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 13:23:56 -0500

    replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = []
+    # empty list
+    for element in new:
+    # looking for search, and replacing with replace
+        if element == search:
+            new.append(replace)
+    # otherwise just append the element
+        else:
+            new.append(element)
+    return (new)
