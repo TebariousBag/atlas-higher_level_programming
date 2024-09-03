4a84d7ca4db5261ee844bda54d98f081e4253b81
“Tristian <tristian.davis@atlasschool.com>
2024-09-03 14:43:57 -0500

    if statement

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max_value = max(a_dictionary, key=a_dictionary.get)
-    return (max_value)
+    if a_dictionary:
+        return (max_value)
     # get the key from adict that is max value
     return (None)
