0945a1a500a079f6d5e610c9a9d595cd3c829bef
“Tristian <tristian.davis@atlasschool.com>
2024-09-07 12:55:13 -0500

    size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Define a class square """
+
+
+class Square:
+    """ Square """
+    def __init__(self, size):
+        """ Initialize square
+        Arg:
+            size: the size of square """
+        self.__size = size
