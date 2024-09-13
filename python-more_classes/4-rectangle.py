6b6bdafae25647f0c71322d720b66174ca57a6ab
“Tristian <tristian.davis@atlasschool.com>
2024-09-12 22:25:16 -0500

    ( )

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
         rectangle = ' '
         if self._width == 0 or self._height == 0:
             return (rectangle)
-        return (f"Rectangle{self._width}, {self._height}")
+        return (f"Rectangle({self._width}, {self._height})")
