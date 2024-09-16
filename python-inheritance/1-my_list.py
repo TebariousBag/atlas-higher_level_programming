86285ebf827363d216067b375de6e109d941a010
“Tristian <tristian.davis@atlasschool.com>
2024-09-16 12:00:30 -0500

    capital

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """ class that inherits from list """
 
 
-class Mylist(list):
+class MyList(list):
     """ prints int list sorted """
     def print_sorted(self):
         """ print in ascending order """
