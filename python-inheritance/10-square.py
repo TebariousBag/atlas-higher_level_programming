85f06e5fd89dd618cb0fe074e1b763147c1b230c
“Tristian <tristian.davis@atlasschool.com>
2024-09-17 10:26:47 -0500

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+    def __init__(self, size):
+        """ size of square """
+        self.integer_validator('size', size)
+        self._size = size
+        
+    def area(self):
+        """ area of square """
+        return self._size * self._size
