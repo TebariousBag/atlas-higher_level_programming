31e32fd521163862f05bee48a6031a5185315d0a
“Tristian <tristian.davis@atlasschool.com>
2024-09-16 20:01:19 -0500

    isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+""" same class or inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """ is instance """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
