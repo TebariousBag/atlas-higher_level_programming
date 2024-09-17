02891e58115c246f6eca1464a7814498a59652e6
“Tristian <tristian.davis@atlasschool.com>
2024-09-16 22:43:45 -0500

    is not

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """ if the object is an instance of a class that inherited """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
