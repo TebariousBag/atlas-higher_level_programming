3dcb5064e9e3fab8c5ae270910b3bb43957db14e
“Tristian <tristian.davis@atlasschool.com>
2024-09-17 00:25:16 -0500

    rect

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-""" Rectangle """
+""" rectangle """
 
 
+class Rectangle(BaseGeometry):
+    """ inherit """
+
+    def __init__(self, width, height):
+        """ new rectangle """
+        self.integer_validator('width', width)
+        self._width = width
+        self.integer_validator('height', height)
+        self_height = height
