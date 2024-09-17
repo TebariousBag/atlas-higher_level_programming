4bd875e808c33d487bfc99c395f7ad147b2992b5
“Tristian <tristian.davis@atlasschool.com>
2024-09-17 10:14:40 -0500

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ inherit """
+
+    def __init__(self, width, height):
+        """ new rectangle """
+        self.integer_validator('width', width)
+        self._width = width
+        self.integer_validator('height', height)
+        self_height = height
+    def area(self):
+        """ area of rectangle """
+        return self._width * self._height
+    
+    def _str_(self):
+        """ string return """
+        return '[Rectangle] {}/{}'.format(self._width, self._height)
