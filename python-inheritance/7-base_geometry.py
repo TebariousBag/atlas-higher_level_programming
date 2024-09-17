4ad0d5199ba072c7585ba27d432cc60daa49acbe
“Tristian <tristian.davis@atlasschool.com>
2024-09-16 23:55:50 -0500

    geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,3 +2,20 @@
 """ Integer Validator """
 
 
+#!/usr/bin/python3
+""" improve geometry """
+
+
+class BaseGeometry:
+    """ raise exception with message """
+
+    def area(self):
+        """ exception with message """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validate value """
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(na,e))
+        if value <= 0:
+            raise ValueError('{} mustbe greater than 0'.format(name))
