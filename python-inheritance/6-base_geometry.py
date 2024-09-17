816cc6d2464b9d85afbb8b936d9fb6751cb4a42e
“Tristian <tristian.davis@atlasschool.com>
2024-09-16 23:40:05 -0500

    exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" improve geometry """
+
+
+class BaseGeometry:
+    """ raise exception with message """
+
+    def area(self):
+        """ exception with message """
+        raise Exception('area() is not implemented')
