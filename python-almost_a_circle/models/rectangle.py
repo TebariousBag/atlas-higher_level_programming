db973364b35495117935917d977e8c6ad8a54f5f
“Tristian <tristian.davis@atlasschool.com>
2024-09-24 15:03:34 -0500

    y setter and some whitespace fixes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """ rectangle from Base """
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ initialize rectangle """
         super().__init__(id)
@@ -13,11 +13,11 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         self.__width = value
@@ -25,7 +25,7 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         self.__height = value
@@ -33,7 +33,7 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         self.__x = value
@@ -41,8 +41,7 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
-    @width.setter
+
+    @y.setter
     def y(self, value):
         self.__y = value
-        
