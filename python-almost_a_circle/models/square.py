bd33bd87acec2fb84520e98c615c3058df25702d
“Tristian <tristian.davis@atlasschool.com>
2024-09-24 20:13:53 -0500

    size not self

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """ square class """
     def __init__(self, size, x=0, y=0, id=None):
         """ square constructor """
-        super().__init__(self, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str represtation """
