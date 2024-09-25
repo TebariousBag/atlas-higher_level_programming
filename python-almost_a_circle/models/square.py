7a17f015f3ca05f322f1827839d93679fe79f056
“Tristian <tristian.davis@atlasschool.com>
2024-09-24 20:12:23 -0500

    return square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ square class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ square constructor """
+        super().__init__(self, size, x, y, id)
+
+    def __str__(self):
+        """ str represtation """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height)
