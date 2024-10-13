91fcf4678abb4dd78ecfe20115b6cd81cdb964d5
“Tristian <tristian.davis@atlasschool.com>
2024-10-13 18:44:20 -0500

    Inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- list all cities in hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
+-- join state_id from cities table and states.id from states table
 INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id
