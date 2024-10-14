133cc2562c69bfd850ec02591cd32fd457291dae
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 18:22:32 -0500

    pycode stuff

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument """
+""" takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument """
 import MySQLdb
 from sys import argv
 
@@ -8,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"
+                .format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
