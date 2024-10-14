d4c775152a654d657e5d529c178edcb213480bc7
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 18:17:28 -0500

    deleted state name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], state_name=argv[4], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE {} ORDER BY id".format(argv[4]))
     rows = cur.fetchall()
