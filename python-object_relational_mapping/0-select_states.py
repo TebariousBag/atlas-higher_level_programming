90ba804b6650d4fb7b9591f01dc4d752c587d08c
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 15:13:36 -0500

    commiting while i try to figure this out

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "--main__":
+	db = MySQLdb.connect(host=localhost, user=MY_USER, passwd=MY_PASS, db=MY_DB port=3306)
+	cur = db.cursor()
+	cur.execute("SELECT * FROM states ORDER BY id")
+	
+
