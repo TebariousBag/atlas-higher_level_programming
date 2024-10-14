5d6896f25ed7cf93a2e4f3bd123638c4ef39cd3b
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 17:56:22 -0500

    extra x in execute

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.exxecute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
     for i in rows:
         print(i)
