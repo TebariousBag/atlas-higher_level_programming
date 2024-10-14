21b1945fc2f794d9c89988156f5c5097a86c204d
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 15:18:58 -0500

    permission denied

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,5 +8,11 @@ if __name__ == "--main__":
 	db = MySQLdb.connect(host=localhost, user=MY_USER, passwd=MY_PASS, db=MY_DB port=3306)
 	cur = db.cursor()
 	cur.execute("SELECT * FROM states ORDER BY id")
-	
-
+	rows = cur.fetchall()
+	for row in rows:
+		for col in row:
+			print(row)
+	# Close all cursors
+	cur.close()
+	# Close all databases
+	db.close()
