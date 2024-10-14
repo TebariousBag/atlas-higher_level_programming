241829b971cfb630d8067e37fedb2c46c3a64912
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 18:20:50 -0500

    single quotes?

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE {} ORDER BY id".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id".format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
