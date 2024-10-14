392162889cc935fa6545b1c53a5f702ca0b75ddb
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 18:16:34 -0500

    format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], state_name=argv[4], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE {state_name} ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE {} ORDER BY id".format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
-        cur.close
-        db.close
+    cur.close
+    db.close
