8af6ff3a0135a72c810c4d0f26aa2648e4ff9c3c
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 16:19:00 -0500

    testing

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,13 @@ import MySQLdb
 import sys
 
 
-if __name__ == "--main__":
-	db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states ORDER BY id")
-	rows = cur.fetchall()
-	for row in rows:
-			print(row)
-	# Close all cursors
-	cur.close()
-	# Close all databases
-	db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
