ecf16162bee6f7091aafc3498eb0f77faab4a9f1
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 20:10:47 -0500

    split long line

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id")
+    cur.execute(("SELECT cities.id, cities.name, states.name "
+                 "FROM cities INNER JOIN states "
+                 "ON states.id=cities.state_id"))
     rows = cur.fetchall()
     for i in rows:
         print(i)
