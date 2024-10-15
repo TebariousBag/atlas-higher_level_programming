4ffc40aa24f294844c7c58c9cf0e7b7838ccfafa
“Tristian <tristian.davis@atlasschool.com>
2024-10-15 15:53:31 -0500

    sep

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.name, citie.name FROM cities \
+    cur.execute("SELECT cities.name FROM cities \
                 INNER JOIN states ON states.id=cities.state_id \
                 WHERE states.name=%s", (argv[4],))
     rows = cur.fetchall()
     for i in rows:
-        print(f"{i[0]}, {i[1]}")
+        print(f"{i[0]} sep=", "")
 
     cur.close
     db.close
