7b95d4390e7bc116ac077af8a120f311163f2114
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 22:19:20 -0500

    gonna restart

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pythoon3
+""" file that contains the class definition of a
+State and an instance Base = declarative_base() """
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+	engine = create_engine()
