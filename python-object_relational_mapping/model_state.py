1f244bf1710ea335b9fcdcc624d2d8a97f4d9d11
“Tristian <tristian.davis@atlasschool.com>
2024-10-15 17:44:52 -0500

    tabs instead of spaces

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,8 +8,8 @@ Base = declarative_base()
 
 
 class State(Base):
-	""" state class """
-	__tablename__ = 'states'
+    """ state class """
+    __tablename__ = 'states'
 
-	id = Column(Integer, primary_key=True, nullable=False)
-	name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
