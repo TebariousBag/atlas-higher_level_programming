ec41bc62dd8efb89f56001434eebaba7ea6b8531
“Tristian <tristian.davis@atlasschool.com>
2024-10-15 17:39:28 -0500

    hmm?

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """ file that contains the class definition of a
 State and an instance Base = declarative_base() """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declaritive import declaritive_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(Base):
