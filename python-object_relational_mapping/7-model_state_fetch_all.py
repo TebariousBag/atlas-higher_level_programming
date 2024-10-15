a795f3ff822e1dc558ed4d1b82f74e547fb6f979
“Tristian <tristian.davis@atlasschool.com>
2024-10-15 18:14:10 -0500

    accidental space

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-     session = Session()
+    session = Session()
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=":")
