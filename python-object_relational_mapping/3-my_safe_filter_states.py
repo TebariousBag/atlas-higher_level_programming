030ca26d464e252fcde884797cbbe3514bcf9aa2
“Tristian <tristian.davis@atlasschool.com>
2024-10-14 20:14:17 -0500

    name like %s?

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"
-                .format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id"
+                (argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
