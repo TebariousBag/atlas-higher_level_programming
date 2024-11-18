8de34fa886239fcaed9c1cecb386dbc6fd62f161
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 04:13:28 +0000

    addition if number

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+// star at first argument of command line
+const args = process.argv.slice(2);
+// convert a and b to int
+// index 0 and 1 since we slice 2
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+// function asked for
+function add(a, b) {
+	  return a + b;
+}
+
+// if a or b is NaN, print NaN
+if (isNaN(a) || isNaN(b)){
+	  console.log('NaN');
+} else {
+	  console.log(add(a, b));
+}
