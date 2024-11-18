14e19903adf411192e00708696fd5effae0fd472
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 03:04:50 +0000

    valu of arg

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// start at first argument of command line
+const args = process.argv.slice(2);
+// if no arg
+if (args.length === 0)
+	{
+  console.log('No argument');
+// if arg than print arg
+} else {
+	  console.log(args[0]);
+}
