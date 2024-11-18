905f64e55e32b93c5675b33116d06a056de87c04
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 03:35:11 +0000

    simpler than it sounded

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+// var to hold the lines
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// iterate through the var
+for (const i of array)	{
+  console.log(i);
+}
