30f6928483224aefaf7510d909440e999701d882
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 03:25:13 +0000

    NaN

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// start at first argument of command line
+const args = process.argv.slice(2);
+// convert arg to int
+const int = parseInt(args[0]);
+// if NotaNumber, print Not a number
+// else print the number
+if (isNaN(int))
+	{
+  console.log('Not a number');
+} else {
+	  console.log(`My number: ${int}`);
+}
