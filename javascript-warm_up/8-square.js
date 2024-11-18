4511c64c7e291d38724fd6ade48912a7aeb6b50a
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 03:59:06 +0000

    print square,

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// start at first argument of command line
+const args = process.argv.slice(2);
+// convert arg to int
+const num = parseInt(args[0]);
+// if NotaNumber, print statement
+if (isNaN(num))
+{
+  console.log('Missing size');
+// iterate and print X, repeat num times
+} else {
+  for (let i = 0; i < num; i++)
+  {
+	console.log('X'.repeat(num));
+  }
+}
