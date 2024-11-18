60db7477b178e7dedf09fd5ca88d91a9c04c9f2d
“Tristian <tristian.davis@atlasschool.com>
2024-11-18 04:54:02 +0000

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// start at first argument of command line
+const args = process.argv.slice(2);
+// convert arg to int
+const num = parseInt(args[0]);
+// function factorial(num)
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+	return 1;
+  }
+  return num * factorial(num - 1);
+}
+//print result
+console.log(factorial(num));
