b3965b8f8f70d2fa9a20c27bb2a898b9645a1cc2
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 19:52:17 +0000

    didn't work with an else statement

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// class rectangle, if w or h is = 0
+// than create empty object
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+}
+// export class
+module.exports = Rectangle;
