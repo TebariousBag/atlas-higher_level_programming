3087bc29b46c417e374bb8012936e6f6a24a4339
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 20:16:31 +0000

    swap values, and multiply by 2

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,33 @@
 #!/usr/bin/node
+// class rectangle, if w or h is = 0
+// than create empty object
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	// iterat through height
+	// print X width times then repeate height times
+    print () {
+		for (let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
+
+	// rotate width and height
+	// just swap values
+	rotate() {
+		[this.width, this.height] = [this.height, this.width];
+	}
+
+	// double width and height
+	double() {
+		this.width *= 2;
+		this.height *= 2;
+	}
+}
+// export class
+module.exports = Rectangle;
