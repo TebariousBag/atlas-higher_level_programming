95e4e722409a1b275e498cb0978c4b88918048a3
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 23:28:29 +0000

    second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// extract args except first 2
+let args = process.argv.slice(2);
+// convert args to Number
+args = args.map(Number);
+// if no args print 0
+if (args.length < 2) {
+  console.log(0);
+} else {
+	// sort args ascending
+	const sorted = args.sort((a, b) => a - b);
+	// print the next to last
+	// which is the second biggest
+	console.log(sorted[sorted.length - 2]);
+}
