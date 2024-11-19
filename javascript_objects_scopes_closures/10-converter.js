d35019e11b1315ae0ff71ccc571a29b61d42c3c1
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 22:09:31 +0000

    this one is confusing

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// convert number from base 10 to another base
+// function given
+exports.converter = function (base) {
+	// inner function to convert number
+  return function (number) {
+	// toString(base) nconverts it to base
+	return number.toString(base);
+  };
+};
