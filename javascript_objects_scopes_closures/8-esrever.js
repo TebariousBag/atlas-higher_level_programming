01f536b9baea91f5eb51f6cf41307700d20df3d7
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 21:31:44 +0000

    list reversed

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// function returns list reversed
+exports.esrever = function (list) {
+	  // variable to hold reversed list
+  const reversed = [];
+  // start at end of the list
+  const listy = list.length - 1;
+  // iterate backwards through the list
+  for (let i = listy; i >= 0; i--) {
+	// each time push the element to reversed
+	reversed.push(list[i]);
+  }
+  // return backwards list
+  return reversed;
+};
