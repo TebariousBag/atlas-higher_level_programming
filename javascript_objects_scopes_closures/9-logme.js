c20730973e1131779444881f486a107d2407520f
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 21:44:04 +0000

    backticks, backticks, i always forget, not single quotes

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// count args already printed
+// and prin the current arg
+let count = 0;
+
+// function given
+exports.logMe = function (item) {
+	// print count, print item
+	// remember bacckticks, not single quotes
+  console.log(`${count}: ${item}`);
+  // increment count
+  count++;
+};
