6906b7cdb594cb00c122e269d2f59d2e4d6f6a81
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 20:27:29 +0000

    create square inherit from Rectangle

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// class square inherit from Rectangle
+// require module 4-rectangle.js
+const Rectangle = require('./4-rectangle');
+
+// class Sqare extends all properties of Rectangle
+class Square extends Rectangle {
+	constructor(size) {
+		// super calls the parent
+		super(size, size);
+	}
+}
+// export class
+module.exports = Square;
