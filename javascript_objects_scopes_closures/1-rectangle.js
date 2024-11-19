725d3efcedd39c148ef9d8e2c62bcbbc8ebde87a
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 19:42:16 +0000

    class rect with width and height

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// class rectangle with width and height
+// this is the current instance of class
+class Rectangle {
+	constructor(w, h) {
+		this.width = w;
+		this.height = h;
+	}
+}
+// export class
+module.exports = Rectangle;
