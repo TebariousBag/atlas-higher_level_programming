f64acf20680724d4fbaa250746ab360d80d8124d
“Tristian <tristian.davis@atlasschool.com>
2024-11-19 20:01:20 +0000

    rectangle but print with X

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// class rectangle, if w or h is = 0
+// than create empty object
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	// iterat through height
+	// print X width times then repeate height times
+    print () {
+		for (let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
+}
+// export class
+module.exports = Rectangle;
