e884712d6406bf45fefbcb85d43a9595ea66898a
“Tristian <tristian.davis@atlasschool.com>
2024-12-01 22:16:46 +0000

    semistandard --fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 // get url from command line
 const url = process.argv[2];
-// id we are looking for
+// id to count
 const id = '18';
 // make a request to GET url, unless error
 request(url, (error, response, body) => {
@@ -14,11 +14,13 @@ request(url, (error, response, body) => {
   // parse body
   const films = JSON.parse(body).results;
   let count = 0;
-  // count goes up for each time id is found
+  // for each film, count character
   films.forEach(film => {
-    if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${id}/`)) {
-      count++;
-    }
+    film.characters.forEach(character => {
+      if (character.includes(`/api/people/${id}/`)) {
+        count++;
+      }
+    });
   });
 
   console.log(count);
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,22 +1,26 @@
 #!/usr/bin/node
 // import request built in
 const requesting = require('request');
-// import fs built in
-const fs = require('fs');
 // get url from command line
 const url = process.argv[2];
-// get file path from command line
-const file = process.argv[3];
 // make a request to GET url, unless error
 requesting(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    // write file, unless error
-    fs.writeFile(file, body, 'utf8', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    console.error('Error:', error);
+    return;
   }
+  // parse body
+  const todos = JSON.parse(body);
+  const tasks = {};
+  // for each completed, tasks increments
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (!tasks[todo.userId]) {
+        tasks[todo.userId] = 0;
+      }
+      tasks[todo.userId]++;
+    }
+  });
+  // print user and completed tasks
+  console.log(JSON.stringify(tasks));
 });
