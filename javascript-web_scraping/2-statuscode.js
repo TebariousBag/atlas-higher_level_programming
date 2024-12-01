7737fe133121b140b361d9c76fdff66bed934582
“Tristian <tristian.davis@atlasschool.com>
2024-12-01 21:19:57 +0000

    request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// import request built in
+const requesting = require('request');
+// get url from command line
+const url = process.argv[2];
+// make a request to GET url, unless error
+requesting(url, (error, response) => {
+  if (error) {
+	console.error('code:', error);
+  } else {
+	console.log('code:', response.statusCode);
+  }
+});
