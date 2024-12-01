ee9f9fde69b2edeaa4382378d6de4fd356f142c1
“Tristian <tristian.davis@atlasschool.com>
2024-12-01 21:09:45 +0000

    readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// import fs builtt in
+const fs = require('fs');
+// get arg2 from command line
+const file = process.argv[2];
+// read the file, unless error occuers
+fs.readFile(file, 'utf8', (err, data) => {
+	  if (err) {
+	console.log(err);
+  } else {
+	console.log(data);
+  }
+});
