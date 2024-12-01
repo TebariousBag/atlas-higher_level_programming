1fd824f05f358f8a9c937aa249fc14625db036de
“Tristian <tristian.davis@atlasschool.com>
2024-12-01 22:01:06 +0000

    how many completed

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// import request built in
+const requesting = require('request');
+// import fs built in
+const fs = require('fs');
+// get url from command line
+const url = process.argv[2];
+// get file path from command line
+const file = process.argv[3];
+// make a request to GET url, unless error
+requesting(url, (error, response, body) => {
+  if (error) {
+	console.error(error);
+  } else {
+	// write file, unless error
+	fs.writeFile(file, body, 'utf8', (err) => {
+	  if (err) {
+		console.log(err);
+	  }
+	});
+  }
+});
---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// import request built in
+const requesting = require('request');
+// get url from command line
+const url = process.argv[2];
+// make a request to GET url, unless error
+requesting(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        return;
+    }
+	// parse body
+    const todos = JSON.parse(body);
+	// empty object to hold completed tasks
+    const tasks = {};
+    // for each completed, tasks increments
+    todos.forEach(todo => {
+        if (todo.completed) {
+            if (!tasks[todo.userId]) {
+                tasks[todo.userId] = 0;
+            }
+            tasks[todo.userId]++;
+        }
+    });
+    // print user id and completed tasks
+    for (const userId in tasks) {
+        console.log(`User ${userId} has completed ${tasks[userId]} tasks`);
+    }
+});
